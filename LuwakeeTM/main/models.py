bcc9d81cbdab7d6acba0cad113b980bb15839663
luwakee <luwakee@yahoo.com>
2023-05-13 01:58:27 +0300

    1st version. The site's appearance will be changed later.

--- LuwakeeTM/main/models.py
+++ LuwakeeTM/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Task(models.Model):
+    title = models.CharField('Name', max_length=50)
+    task = models.TextField('Despription')
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
